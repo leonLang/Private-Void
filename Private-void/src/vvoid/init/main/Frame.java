b786284571f0303cace9a502ad2c46d461eaf412
Peter <trompeten.peter@gmx.de>
2018-04-01 13:44:09 +0200

    fgh

--- Private-void/src/vvoid/init/main/Frame.java
+++ Private-void/src/vvoid/init/main/Frame.java
@@ -1,5 +1,8 @@
 package vvoid.init.main;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Frame extends JFrame {
@@ -7,9 +10,16 @@ public class Frame extends JFrame {
 	private static final long serialVersionUID = 9019603114311641540L;
 
 	public Frame(int WIDTH,int HEIGHT,String title,boolean Resizable) {
+		//get screen size
+		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+		int screenWIDTH = (int) screen.getWidth();
+		int screenHEIGHT = (int) screen.getHeight();
+		
+		
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLocationRelativeTo(null);
+		//setLocationRelativeTo(null);
+		setLocation(screenWIDTH/2-WIDTH/2,screenHEIGHT/2-HEIGHT/2);
 		
 		setTitle(title);
 		setSize(WIDTH, HEIGHT);
