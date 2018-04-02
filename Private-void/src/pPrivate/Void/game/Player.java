996e77c4bff4252d69264cd0e60a7059db0dd5e9
Peter <trompeten.peter@gmx.de>
2018-04-02 12:28:44 +0200

    Key

--- Private-void/src/pPrivate/Void/game/Player.java
+++ Private-void/src/pPrivate/Void/game/Player.java
@@ -1,12 +1,29 @@
 package pPrivate.Void.game;
 
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Player extends Game{
+public class Player extends Game implements KeyListener{
 	public Player() {
 		
 	}
 	public static void drawPlayer(Graphics g) {
 		g.drawRect(200, 200, 100, 100);
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
