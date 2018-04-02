f3cd542520f4a51fee9f32eedef8c8617e196177
Peter <trompeten.peter@gmx.de>
2018-04-02 16:14:36 +0200

    sdf

--- Private-void/src/pPrivate/Void/game/Game.java
+++ Private-void/src/pPrivate/Void/game/Game.java
@@ -4,7 +4,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 
 
-import vvoid.init.main.Camara;
+import vvoid.init.main.Camera;
 import vvoid.init.main.Collision;
 import vvoid.init.main.Frame;
 
