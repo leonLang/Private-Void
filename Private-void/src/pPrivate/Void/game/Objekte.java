3e52e36800174562f0f8b4624187d4264f0b024b
Peter <trompeten.peter@gmx.de>
2018-04-02 16:14:13 +0200

    jkh

--- Private-void/src/pPrivate/Void/game/Objekte.java
+++ Private-void/src/pPrivate/Void/game/Objekte.java
@@ -3,10 +3,10 @@ package pPrivate.Void.game;
 import java.awt.Graphics;
 
 public class Objekte {
-	public static int E1Width=100;
-	public static int E1Height=150;
-	public static int E2Width=100;
-	public static int E2Height=150;
+	public static int E1Width=200;
+	public static int E1Height=10;
+	public static int E2Width=200;
+	public static int E2Height=100;
 	public static void enemy1(int x, int y,Graphics g) {
 		g.drawRect(x, y, E1Width, E1Height);
 	}
