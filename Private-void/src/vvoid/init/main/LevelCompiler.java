08fe13981310767ba439a9477ceff1afa64fd1a1
Peter <trompeten.peter@gmx.de>
2018-04-02 19:15:56 +0200

    apiu

--- Private-void/src/vvoid/init/main/LevelCompiler.java
+++ Private-void/src/vvoid/init/main/LevelCompiler.java
@@ -2,26 +2,50 @@ package vvoid.init.main;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class LevelCompiler {
-	public static int hex = 0x55;
 	public LevelCompiler() {
-		print();
+
 		read();
 	}
-	public void print() {
-		System.out.println(hex);
-	}
+
 	public void read() {
+
+
 		try {
-		File file = new File("/Private-void/src/Level/level.pll");
+		File file = new File("Level/level.pll");
 		BufferedReader br = new BufferedReader(new FileReader(file));
-		  String st;
-		  while ((st = br.readLine()) != null)
-		    System.out.println(st);
+		  String st = br.readLine();
+		  if(st.contains("!pllDOCUMENT")) {
+		  while ((st = br.readLine()) != null) {
+			  if(st.contains("//") != true) {
+
+				  int[] Object= {
+						  		 convert(st.substring(0, 1)),
+						  		 convert(st.substring(3, 7)),
+						  		 convert(st.substring(8, 12)),
+						  		 convert(st.substring(13, 17)),
+						  		 convert(st.substring(18, 21)),
+						  		 convert(st.substring(22, 26))
+						  		 };
+				    System.out.println(st);
+				    System.out.println(Arrays.toString(Object));
+			  	}
+		  	}
+		  } else {
+			  System.out.println("this is not a pll file");
+		  }
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
+	public int convert(String s) {
+		int p = Integer.parseInt(s, 16);
+		return p;
+	}
+
 }
