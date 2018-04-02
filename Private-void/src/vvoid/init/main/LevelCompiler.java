dc4cd0a98fd1a7e56943581d80d185f593d33fb4
Peter <trompeten.peter@gmx.de>
2018-04-02 16:49:31 +0200

    hzgf

--- Private-void/src/vvoid/init/main/LevelCompiler.java
+++ Private-void/src/vvoid/init/main/LevelCompiler.java
@@ -1,11 +1,27 @@
 package vvoid.init.main;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class LevelCompiler {
 	public static int hex = 0x55;
 	public LevelCompiler() {
 		print();
+		read();
 	}
 	public void print() {
 		System.out.println(hex);
+	}
+	public void read() {
+		try {
+		File file = new File("./Private-void/GameLevel/level.pll");
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		  String st;
+		  while ((st = br.readLine()) != null)
+		    System.out.println(st);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
