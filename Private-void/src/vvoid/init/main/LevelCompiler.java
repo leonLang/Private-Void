fb8033d57e3a37c30f9a7522c798164678aaf3bd
Peter <trompeten.peter@gmx.de>
2018-04-03 13:27:30 +0200

    sdf

--- Private-void/src/vvoid/init/main/LevelCompiler.java
+++ Private-void/src/vvoid/init/main/LevelCompiler.java
@@ -3,16 +3,10 @@ package vvoid.init.main;
 import java.awt.Image;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.EmptyStackException;
-
 import javax.imageio.ImageIO;
-
-import pPrivate.Void.game.Objekte;
 
 public class LevelCompiler {
 	private String path;
@@ -34,12 +28,12 @@ public class LevelCompiler {
 			  if(st.contains("//") != true) {
 
 				  int[] Object= {
-						  		 convertB(st.substring(0, 2)),
-						  		 convertH(st.substring(3, 7)),
-						  		 convertH(st.substring(8, 12)),
-						  		 convertH(st.substring(13, 17)),
-						  		 convertD(st.substring(18, 21)),
-						  		 convertH(st.substring(22, 26))
+						  		 convertB(st.substring(0, 2)),//ID
+						  		 convertH(st.substring(3, 7)),//IDO
+						  		 convertH(st.substring(8, 12)),//X-Coordinate
+						  		 convertH(st.substring(13, 17)),//Y-Coordinate
+						  		 convertD(st.substring(18, 21)),//Rotation
+						  		 convertH(st.substring(22, 26))//TextureID
 						  		 };
 				    System.out.println(st);
 				    System.out.println(Arrays.toString(Object));
@@ -73,7 +67,7 @@ public class LevelCompiler {
 			createBackground(Object[1]);
 			break;
 		case 1:
-			createObject();
+			createObject(Object[1],Object[2],Object[3],Object[4],Object[5]);
 			break;
 		case 2:
 			break;
@@ -91,11 +85,15 @@ public class LevelCompiler {
 		}
 		System.out.println(path);
 	}
-	private void createObject() {
-		
-	}
 	public static Image getbackground() {
 		return img;
+	}
+	
+	private void createObject(int ID, int x, int y, int rotation, int TextureID) {
+		
+	}
+	private void createEnemie() {
+		
 	}
 
 }
