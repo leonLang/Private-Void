df37ae6ca10c4e3aa93e92a168f1fc1a4a11ef21
leon <leon@leon-pc>
2018-04-03 22:21:57 +0200

    ffffffff

--- Private-void/src/pPrivate/Void/game/Enemie.java
+++ Private-void/src/pPrivate/Void/game/Enemie.java
@@ -9,23 +9,30 @@ public class Enemie {
 	private int width;
 	private int height;
 	private int TextureID;
-
+	public static int IDO;
+	public int zaehler =1;
 	
 	public Enemie(int IDO, int x, int y, int rotation, int TextureID) {
 		this.x = x;
 		this.y = y;
 		this.TextureID = TextureID;
 		size(IDO);
+		System.out.println(IDO);
 	}
 	private void size(int IDO) {
 		switch(IDO) {
 		case 1:
+			Enemie.IDO = IDO;
+
 			width = 100;
 			height = 100;
+			System.out.println(Enemie.IDO);
+
 			break;
 		case 2:
+			Enemie.IDO = IDO;
 			width = 100;
-			height = 200;
+			height = 100;
 			break;
 			//feel free to add more Dimensions :)
 		default:
@@ -35,9 +42,16 @@ public class Enemie {
 	}
 
 	public void drawEnemie(Graphics g) {
+		if (Enemie.IDO == 1) {
         g.drawRect(x, y, width, height);
         g.drawImage(sheet.crop(0, 0, 25, 25),x,y,width,height,null);
-       // g.drawImage(sheet.crop(0, 0, 32, 32),100,100,100,100,null);		
+       // g.drawImage(sheet.crop(0, 0, 32, 32),100,100,100,100,null);
+		}
+		if (Enemie.IDO==2) {
+			  g.drawRect(x, y, width, height);
+		      g.drawImage(sheet.crop(25, 25, 25, 25),x,y,width,height,null);
+		       
+		}
 	}
 
 
