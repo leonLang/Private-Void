bc74f05331cbfcee488e907daadfe7a5d32bd1d6
leon <leon@leon-pc>
2018-04-05 13:14:03 +0200

    xxxxxxxx

--- Private-void/src/vvoid/map/objects/Enemie.java
+++ Private-void/src/vvoid/map/objects/Enemie.java
@@ -40,6 +40,16 @@ public class Enemie {
        
 		}
 		}
+		switch(this.IDO) {
+				case 1:
+					sh(g, 9, 7, 16, 14, 27, 7, 16, 14);
+					break;
+					
+				case 2:
+					sh(g, 25, 25, 25, 25, 50, 25, 25, 25);
+					//noch en tanzendes Sprit als freude für dich morgen xD
+					break;
+		 		}
 	
 		
 		
@@ -49,6 +59,8 @@ public class Enemie {
 		       
 		}
 	}
-
+	public void sh(Graphics g, int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2) {
+		EnemieMovement.movement(g, x1, y1, w1, h1, x2, y2,w2, h2, x, y, width, height);
+		}
 
 }
