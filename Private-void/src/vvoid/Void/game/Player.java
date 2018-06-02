355e94cdf845a9e33ddb2fc015475d2832a079eb
Peter <trompeten.peter@gmx.de>
2018-06-02 13:32:11 +0200

    asdf

--- Private-void/src/vvoid/Void/game/Player.java
+++ Private-void/src/vvoid/Void/game/Player.java
@@ -9,14 +9,14 @@ public class Player {
 	public boolean l;
 	public boolean r;
 	public String facing;
-	public int   x;
+	public int x;
 	public int y;
 	public int XSolid;
 	public int YSolid;
 	public int width;
 	public int height;
-	
-	public Player(int x, int y, int width, int height,String facing) {
+
+	public Player(int x, int y, int width, int height, String facing) {
 		this.x = x;
 		this.y = y;
 		this.XSolid = x;
@@ -25,60 +25,70 @@ public class Player {
 		this.height = height;
 		this.facing = facing;
 		new Pgravity();
-		
+
 		this.l = false;
 		this.r = false;
-		
+
 		(new Thread(new Pgravity())).start();
 	}
 
 	public void drawPlayer(Graphics g) {
 		g.drawRect(this.XSolid, this.YSolid, this.width, this.height);
-		//System.out.println(x);
+		// System.out.println(x);
 	}
 
 	public void Move(String c) {
 		(new Thread(new movment(c))).start();
 	}
+
 	public String getfacing() {
 		return facing;
 	}
+
 	public int getX() {
 		return x;
 	}
+
 	public int getY() {
 		return y;
 	}
+
 	public int getWidth() {
 		return width;
 	}
+
 	public int getHeight() {
 		return height;
 	}
+
 	public void UP() {
 		Game.player.addY(Camera.step);
 	}
+
 	public void DOWN() {
 		Game.player.addY(-Camera.step);
 	}
+
 	public void addX(int i) {
 		this.x -= i;
 	}
+
 	public void addY(int i) {
 		this.y -= i;
 	}
 
 	private class movment implements Runnable {
 		private String c;
+
 		public movment(String c) {
 			this.c = c;
 		}
 
 		public void MoveLeft() {
-			
+
 			facing = "left";
 			while (l) {
-				if(EOArrey.linksP == true) {
+				if (EOArrey.linksP == true) {
 					l = false;
 				} else {
 					Camera.addx(-Camera.step);
@@ -89,18 +99,18 @@ public class Player {
 						e.printStackTrace();
 					}
 				}
-				if(EOArrey.rechtsP == true) {
+				if (EOArrey.rechtsP == true) {
 				}
-				
+
 			}
 		}
 
 		public void MoveRight() {
-			facing ="right";
+			facing = "right";
 			while (r) {
-				if(EOArrey.linksP == true) {
+				if (EOArrey.linksP == true) {
 				}
-				if(EOArrey.rechtsP == true) {
+				if (EOArrey.rechtsP == true) {
 					r = false;
 				} else {
 					Camera.addx(Camera.step);
@@ -116,7 +126,7 @@ public class Player {
 
 		@Override
 		public void run() {
-			
+
 			switch (c) {
 			case "l":
 				MoveLeft();
@@ -128,10 +138,11 @@ public class Player {
 			Thread.currentThread().interrupt();
 		}
 	}
-	private class Pgravity implements Runnable{
+
+	private class Pgravity implements Runnable {
 
 		private void gravity() {
-			while(EOArrey.untenP != true) {
+			while (EOArrey.untenP != true) {
 				Camera.addy(Camera.step);
 				Game.player.addY(-Camera.step);
 				try {
@@ -140,12 +151,20 @@ public class Player {
 					e.printStackTrace();
 				}
 			}
+			try {
+				Thread.sleep(20);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			gravity();
+			
 		}
+
 		@Override
 		public void run() {
 			System.out.println("hier bin ich");
 			gravity();
-			
+
 		}
 	}
 }
