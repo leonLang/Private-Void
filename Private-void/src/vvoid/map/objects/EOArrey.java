d018774a2dd8c020d407b9ae6df55ef8bd69cbc6
leonl <leonl@leon-pc>
2018-06-02 14:01:30 +0200

    dddd

--- Private-void/src/vvoid/map/objects/EOArrey.java
+++ Private-void/src/vvoid/map/objects/EOArrey.java
@@ -248,7 +248,7 @@ public class EOArrey {
 		// System.out.println(xO[1]);
 	}
 
-	public void CollPlayer() {
+	public synchronized void CollPlayer() {
 		counterP = oAmount; // for editing oAmount without change it
 		while (counterP >= 0) {
 			coll = new Collision(Game.player.x, Game.player.y, Game.player.width, Game.player.height, xO[counterP],
