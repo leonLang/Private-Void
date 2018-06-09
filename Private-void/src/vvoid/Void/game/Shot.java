03a4ff046560805f7e43ac1431f35041fe1e85a6
Peter <trompeten.peter@gmx.de>
2018-06-09 15:53:33 +0200

    asdf

--- Private-void/src/vvoid/Void/game/Shot.java
+++ Private-void/src/vvoid/Void/game/Shot.java
@@ -1,11 +1,23 @@
 package vvoid.Void.game;
 
+import java.awt.Graphics;
+
 public class Shot {
 	public String direction;
 	public int x;
 	public int y;
+	public int width;
+	public int height;
 
 	public Shot(String getfacing,int x, int y) {
 		this.direction = getfacing;
+		this.x = x;
+		this.y = y;
+		width = 30;
+		height = 30;
+		System.out.println("hi");
+	}
+	public void draw(Graphics g) {
+		g.drawRect(x, y, width, height);
 	}
 }
