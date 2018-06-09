b6be1d8188447e4dcb2900ec40755f0887da0ebe
leonl <leonl@leon-pc>
2018-06-09 15:58:15 +0200

    ffff

--- Private-void/src/vvoid/init/main/Main.java
+++ Private-void/src/vvoid/init/main/Main.java
@@ -34,7 +34,7 @@ public class Main {
 		final int MAX_UPDATES_BEFORE_RENDER = 5;
 		double lastUpdateTime = System.nanoTime();
 		double lastRenderTime = System.nanoTime();
-		final double TARGET_FPS = 60;
+		final double TARGET_FPS = 100;
 		final double TARGET_TIME_BETWEEN_RENDERS = 1000000000 / TARGET_FPS;
 		int lastSecondTime = (int) (lastUpdateTime / 1000000000);
 
