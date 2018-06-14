f0a31a96bcc0a8b4cd0cbbcef1031b2d2354ec50
leonl <leonl@leon-pc>
2018-06-14 23:54:29 +0200

    vvv

--- Private-void/src/vvoid/Void/collision/EnemieColl.java
+++ Private-void/src/vvoid/Void/collision/EnemieColl.java
@@ -5,7 +5,6 @@ import vvoid.Void.game.Game;
 import vvoid.init.main.Collision;
 
 public class EnemieColl {
-
 	private Collision coll;
 	private int oben, rechts, unten, links;
 	private int counter, start, time;
@@ -21,7 +20,6 @@ public class EnemieColl {
 	public static int[] yE = new int[10000]; // y-postion (blocks)
 	public static int eAmount = -1; // number of blocks (-1 is for the array who starts with 0)
 
-	
 	public void enemyS(int width, int height, int x, int y, int id) {
 		eAmount++;
 		widthE[eAmount] = width;
@@ -36,208 +34,27 @@ public class EnemieColl {
 	}
 
 	public void enemyR() {
-		// System.out.println(Game.player.getY());
-
-		/*
-		 * Change widhtO to +3 so you can detect if enemy is right from the block in the
-		 * start Change enemy width to 50 so you can detect if enemy is left from the
-		 * block in the start Change heightO to +16 to detect the enemy under the block
-		 */
 		counter = ObjektColl.oAmount; // for editing oAmount without change it
 		o = false;
 		r = false;
 		l = false;
 		while (counter >= 0) {
-			if (id >= 5 && id <= 14 || id >= 18 && id <= 20 || id >= 23 && id <= 25) {
-				coll = new Collision(this.x, this.y, this.width, this.height, ObjektColl.xO[counter],
-						ObjektColl.yO[counter], ObjektColl.widthO[counter], ObjektColl.heightO[counter]);
-				if (coll.CollRechts() == true) {
-					direction = false;
-					counter = -1;
-				}
-				if (coll.CollLinks() == true) {
-					direction = true;
-					counter = -1;
-				}
-				counter--;
-			} else {
-				// Only do this in the beginning
-				if (start == 0) {
-					coll = new Collision(this.x, this.y, this.width, this.height, ObjektColl.xO[counter],
-							ObjektColl.yO[counter], ObjektColl.widthO[counter], ObjektColl.heightO[counter]);
 
-					if (coll.Coll1() == true) {
-						// Check if block has collision from above
-						start = 1;
-					} else {
-						coll = new Collision(this.x, this.y, this.width, this.height, ObjektColl.xO[counter],
-								ObjektColl.yO[counter], ObjektColl.widthO[counter], ObjektColl.heightO[counter] + 16);
-						if (coll.Coll1() == true) {
-							// Check if block has collision from below
-							y = y - 16;
-							start = 1;
-						} else {
-							coll = new Collision(this.x, this.y, 50, this.height, ObjektColl.xO[counter],
-									ObjektColl.yO[counter], ObjektColl.widthO[counter], ObjektColl.heightO[counter]);
-							if (coll.Coll1() == true) {
-								// Check if block has collision from left
-								x = x + 13;
-								start = 1;
+			if (checkIfFly() != true) {
 
-							} else {
-								coll = new Collision(this.x, this.y, this.width, this.height, ObjektColl.xO[counter],
-										ObjektColl.yO[counter], ObjektColl.widthO[counter] + 3,
-										ObjektColl.heightO[counter]);
-								if (coll.Coll1() == true) {
-
-									x = x - 3;
-									start = 1;
-									// Check if block has collision from right
-								} else {
-									// y++;
-									// if it has no Collision move 1pixel below
-								}
-							}
-						}
-					}
-				}
-
+				startPos();
 				coll = new Collision(this.x, this.y, this.width, this.height, ObjektColl.xO[counter],
 						ObjektColl.yO[counter], ObjektColl.widthO[counter], ObjektColl.heightO[counter]);
 				counter--;
+				collUp();
+				collRight();
+				collDown();
+				collLeft();
 
-				if (coll.CollOben() == true) {
-					/*
-					 * when it moves right there are two possibilites first: move up next:move down
-					 * you can only easily detect the collision up. To see if you have to move up
-					 * you have to check if the now used Collision is over and then move one pixel
-					 * down
-					 */
-					up = true;
-					oben = 0;
-					if (coll.CollLinks() == true) {
-						y--;
-						up = false;
-						x = x - 2;
-						oben = 0;
-						// Only to check if if it goes up
-					} else {
-						if (o == false) {
-							o = true;
-							richtung = 0;
-							x++;
-						}
-					}
-					// the code checks, that he only moves down if the collision is over
-				} else {
-					oben++;
-				}
-				if (up == true) {
-
-					if (oben >= ObjektColl.oAmount * 3) {
-						y = y + 1;
-						up = false;
-					}
-				}
-
-				if (coll.CollRechts() == true) {
-					/*
-					 * when it moves down there are two possibilites first: move right next:move
-					 * left you can only easily detect the collision right. To see if you have to
-					 * move left you have to check if the now used Collision is over and then move
-					 * one pixel left
-					 */
-					right = true;
-					rechts = 0;
-					if (coll.CollOben() == true) {
-						x++;
-						y--;
-						right = false;
-						rechts = 0;
-					} else {
-						if (r == false) {
-							richtung = 1;
-							r = true;
-							y++;
-						}
-					}
-
-				} else {
-					rechts++;
-				}
-				if (right == true) {
-					if (rechts >= ObjektColl.oAmount * 3) {
-						x--;
-						right = false;
-					}
-				}
-
-				if (coll.CollUnten() == true) {
-					/*
-					 * when it moves left there are two possibilites first: move down next:up left
-					 * you can only easily detect the collision down. To see if you have to move up
-					 * you have to check if the now used Collision is over and then move one pixel
-					 * up
-					 */
-					down = true;
-					unten = 0;
-					if (coll.CollRechts() == true) {
-						y++;
-						x++;
-						down = false;
-						unten = 0;
-					} else {
-						if (o == false) {
-							o = true;
-							richtung = 2;
-							x--;
-						}
-					}
-
-				} else {
-					unten++;
-				}
-				if (down == true) {
-					if (unten >= ObjektColl.oAmount * 3) {
-						y--;
-						down = false;
-					}
-				}
-
-				if (coll.CollLinks() == true) {
-					left = true;
-					links = 0;
-					if (coll.CollUnten() == true) {
-						links = 0;
-						left = false;
-						x--;
-						y++;
-					} else {
-						if (l == false) {
-							l = true;
-							y--;
-							richtung = 3;
-						}
-					}
-				} else {
-					links++;
-				}
-				if (left == true) {
-					if (links >= ObjektColl.oAmount * 3) {
-						x++;
-						left = false;
-					}
-				}
 			}
+
 		}
-		if (id >= 5 && id <= 14 || id >= 18 && id <= 20 || id >= 23 && id <= 25) {
-			if (direction != true) {
-				x++;
-			} else {
-				x--;
-			}
-			counter = -1;
-		}
+		fly();
 	}
 
 	public void updateE(int idE, int x, int y) {
@@ -268,6 +85,219 @@ public class EnemieColl {
 			if (coll.Coll1() == true) {
 				Game.leben--;
 				time = 120;
+			}
+		}
+	}
+
+	private void startPos() {
+		/*
+		 * Change widhtO to +3 so you can detect if enemy is right from the block in the
+		 * start Change enemy width to 50 so you can detect if enemy is left from the
+		 * block in the start Change heightO to +16 to detect the enemy under the block
+		 */
+		if (start == 0) {
+			coll = new Collision(this.x, this.y, this.width, this.height, ObjektColl.xO[counter],
+					ObjektColl.yO[counter], ObjektColl.widthO[counter], ObjektColl.heightO[counter]);
+
+			if (coll.Coll1() == true) {
+				// Check if block has collision from above
+				start = 1;
+			} else {
+				coll = new Collision(this.x, this.y, this.width, this.height, ObjektColl.xO[counter],
+						ObjektColl.yO[counter], ObjektColl.widthO[counter], ObjektColl.heightO[counter] + 16);
+				if (coll.Coll1() == true) {
+					// Check if block has collision from below
+					y = y - 16;
+					start = 1;
+				} else {
+					coll = new Collision(this.x, this.y, 50, this.height, ObjektColl.xO[counter],
+							ObjektColl.yO[counter], ObjektColl.widthO[counter], ObjektColl.heightO[counter]);
+					if (coll.Coll1() == true) {
+						// Check if block has collision from left
+						x = x + 13;
+						start = 1;
+
+					} else {
+						coll = new Collision(this.x, this.y, this.width, this.height, ObjektColl.xO[counter],
+								ObjektColl.yO[counter], ObjektColl.widthO[counter] + 3, ObjektColl.heightO[counter]);
+						if (coll.Coll1() == true) {
+
+							x = x - 3;
+							start = 1;
+							// Check if block has collision from right
+						} else {
+							// y++;
+							// if it has no Collision move 1pixel below
+						}
+					}
+				}
+			}
+		}
+	}
+
+	private boolean checkIfFly() {
+		if (id >= 5 && id <= 14 || id >= 18 && id <= 20 || id >= 23 && id <= 25) {
+			coll = new Collision(this.x, this.y, this.width, this.height, ObjektColl.xO[counter],
+					ObjektColl.yO[counter], ObjektColl.widthO[counter], ObjektColl.heightO[counter]);
+			if (coll.CollRechts() == true) {
+				direction = false;
+				counter = -1;
+			}
+			if (coll.CollLinks() == true) {
+				direction = true;
+				counter = -1;
+			}
+			counter--;
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	private void fly() {
+		if (id >= 5 && id <= 14 || id >= 18 && id <= 20 || id >= 23 && id <= 25)
+
+		{
+			if (direction != true) {
+				x++;
+			} else {
+				x--;
+			}
+			counter = -1;
+		}
+
+	}
+
+	private void collUp() {
+		if (coll.CollOben() == true) {
+			/*
+			 * when it moves right there are two possibilites first: move up next:move down
+			 * you can only easily detect the collision up. To see if you have to move up
+			 * you have to check if the now used Collision is over and then move one pixel
+			 * down
+			 */
+			up = true;
+			oben = 0;
+			if (coll.CollLinks() == true) {
+				y--;
+				up = false;
+				x = x - 2;
+				oben = 0;
+				// Only to check if if it goes up
+			} else {
+				if (o == false) {
+					o = true;
+					richtung = 0;
+					x++;
+				}
+			}
+			// the code checks, that he only moves down if the collision is over
+		} else {
+			oben++;
+		}
+		if (up == true) {
+
+			if (oben >= ObjektColl.oAmount * 3) {
+				y = y + 1;
+				up = false;
+			}
+		}
+	}
+
+	private void collDown() {
+		if (coll.CollUnten() == true) {
+			/*
+			 * when it moves left there are two possibilites first: move down next:up left
+			 * you can only easily detect the collision down. To see if you have to move up
+			 * you have to check if the now used Collision is over and then move one pixel
+			 * up
+			 */
+			down = true;
+			unten = 0;
+			if (coll.CollRechts() == true) {
+				y++;
+				x++;
+				down = false;
+				unten = 0;
+			} else {
+				if (o == false) {
+					o = true;
+					richtung = 2;
+					x--;
+				}
+			}
+
+		} else {
+			unten++;
+		}
+		if (down == true) {
+			if (unten >= ObjektColl.oAmount * 3) {
+				y--;
+				down = false;
+			}
+		}
+
+	}
+
+	private void collRight() {
+		if (coll.CollRechts() == true) {
+			/*
+			 * when it moves down there are two possibilites first: move right next:move
+			 * left you can only easily detect the collision right. To see if you have to
+			 * move left you have to check if the now used Collision is over and then move
+			 * one pixel left
+			 */
+			right = true;
+			rechts = 0;
+			if (coll.CollOben() == true) {
+				x++;
+				y--;
+				right = false;
+				rechts = 0;
+			} else {
+				if (r == false) {
+					richtung = 1;
+					r = true;
+					y++;
+				}
+			}
+
+		} else {
+			rechts++;
+		}
+		if (right == true) {
+			if (rechts >= ObjektColl.oAmount * 3) {
+				x--;
+				right = false;
+			}
+		}
+
+	}
+
+	private void collLeft() {
+		if (coll.CollLinks() == true) {
+			left = true;
+			links = 0;
+			if (coll.CollUnten() == true) {
+				links = 0;
+				left = false;
+				x--;
+				y++;
+			} else {
+				if (l == false) {
+					l = true;
+					y--;
+					richtung = 3;
+				}
+			}
+		} else {
+			links++;
+		}
+		if (left == true) {
+			if (links >= ObjektColl.oAmount * 3) {
+				x++;
+				left = false;
 			}
 		}
 	}
